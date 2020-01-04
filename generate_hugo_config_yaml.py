66f05f5d49ad0b29969d2317168202dd1bb30324
Hugo O. Rivera <6373447+roguh@users.noreply.github.com>
2020-01-03 20:33:28 -0800

    Order and weight languages by regions/roots

--- generate_hugo_config_yaml.py
+++ generate_hugo_config_yaml.py
@@ -4,33 +4,42 @@
 import cache
 import data
 import flag
-import progressbar
 from googletrans import Translator
+from progressbar import ProgressBar
 
+translator = Translator()
+memo = cache.PersistentMemoizationCache(
+    cache_name='generate_hugo_config_yaml'
+)
+sort = False
+sort_or_not = sorted if sort else (lambda x: x)
+
+secondary_weight = 5
 
 def main():
     """Translate and render."""
-    translator = Translator()
-    memo = cache.PersistentMemoizationCache(
-        cache_name='generate_hugo_config_yaml'
-    )
-    sort = False
-    sort_or_not = sorted if sort else (lambda x: x)
+    weight = 0
 
-    with open('error.log', 'w') as f:
-        for lang_code, lang_data in progressbar.progressbar(
-                sort_or_not(data.languages.items())
-        ):
+    i = 0
+    bar = ProgressBar(max_value=len(data.languages))
+
+    for root, languages in data.languages_with_roots.items():
+        weight += 10
+        for code, lang_data in sort_or_not(languages.items()):
+            bar.update(i)
+            i += 1
+
             lang_name = lang_data.split(maxsplit=2)[0].replace('_', ' ')
 
-            lang_weight = 3 if lang_code in data.secondary_languages else 4
+            lang_weight = secondary_weight \
+                if code in data.secondary_languages else weight
 
             flag_codes = lang_data.split()[1:]
             emoji_unneeded = any(
                 flag in data.no_flag_needed for flag in flag_codes)
 
             if len(flag_codes) == 0:
-                flag_codes = [lang_code]
+                flag_codes = [code]
             elif len(flag_codes) == 1 and emoji_unneeded:
                 flag_codes = []
 
@@ -39,36 +48,36 @@ def main():
                              subregions=True)
 
             # Something about the UTF-8 encoding of the Ukrainian flag...
-            flag_sep = '"' if any(c in 'ua' for c in lang_code) else "'"
+            flag_sep = '"' if any(c in 'ua' for c in code) else "'"
 
             header_text = data.default_header_text
             site_title = data.default_site_title
 
-            if lang_code in data.lang_code_to_googletrans_code:
-                dest_lang = data.lang_code_to_googletrans_code[lang_code]
+            if code in data.lang_code_to_googletrans_code:
+                dest_lang = data.lang_code_to_googletrans_code[code]
             else:
-                dest_lang = lang_code
+                dest_lang = code
 
-            if lang_code in data.lang_with_separator:
-                separator = data.lang_with_separator[lang_code]
+            if code in data.lang_with_separator:
+                separator = data.lang_with_separator[code]
             else:
                 separator = '  '
 
             try:
-                if lang_code in data.site_title_translations:
-                    site_title = data.site_title_translations[lang_code]
+                if code in data.site_title_translations:
+                    site_title = data.site_title_translations[code]
                 else:
                     site_title = memo.call(translator.translate,
                                            data.site_title,
                                            src='en', dest=dest_lang).text
             except ValueError as e:
-                print(f'failed to translate title for {lang_code}', e, file=f)
-                lang_weight = 3 if lang_code in data.secondary_languages else 9
+                with open('error.log', 'a') as f:
+                    print(f'failed to translate title for {code}', e, file=f)
 
             try:
-                if lang_code in data.phrases_translations:
+                if code in data.phrases_translations:
                     header_text = '{}   {}'.format(
-                        separator.join(data.phrases_translations[lang_code]),
+                        separator.join(data.phrases_translations[code]),
                         data.default_header_text)
                 else:
                     header_text = separator.join(
@@ -77,18 +86,20 @@ def main():
                         for phrase in data.phrases)
                     header_text = f"[{header_text}](/)"
 
-                if lang_code in ['it', 'corsica']:
+                if code in ['it', 'corsica']:
                     header_text = header_text.replace('americana', 'americano')
-                if lang_code == 'zulu':
+                if code == 'zulu':
                     header_text = header_text.replace('Unicode', '')
-                if lang_code == 'latin':
+                if code == 'latin':
                     header_text = header_text.replace('Mexicanus', '')\
                         .replace('-American', 'Mexicanus-American')
             except ValueError as e:
-                print(f'failed to translate language={lang_code}', e, file=f)
-                lang_weight = 3 if lang_code in data.secondary_languages else 9
+                with open('error.log', 'a') as f:
+                    print(f'failed to translate language={code}', e, file=f)
+                lang_weight = secondary_weight \
+                    if code in data.secondary_languages else weight + 5
 
-            print(f"""  '{lang_code}':
+            print(f"""  '{code}':
     languageName: {flag_sep}{lang_name} {flag_emojis}{flag_sep}
     author: "{data.long_author}"
     title: "{site_title} ({data.short_author})"
